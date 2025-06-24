4ee7dd89fe60ec25f30909d04e8d7363841d0210
Tjardo <tjardo.antonie@gmail.com>
2025-06-24 15:13:51 +0200

    grid home-page added

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,11 +1,18 @@
 import Image from "next/image";
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import Grid from "@/components/Grid";
+import { House } from "lucide-react";
 
 export default function Home() {
   return (
-    <main className="relative flex justify-center items-center h-screen bg-black-100 flex-col flex-overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative flex justify-center items-center bg-black-100 flex-col overflow-x-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full text-center">
+        <FloatingNav
+          navItems={[{ name: "Home", link: "/", icon: <House /> }]}
+        />
         <Hero />
+        <Grid />
       </div>
     </main>
   );
--- portfolio/src/components/Hero.tsx
+++ portfolio/src/components/Hero.tsx
@@ -8,14 +8,24 @@ const Hero = () => {
     <div className="pb-20 pt-36">
       <div>
         <Spotlight
-          className="top-40 left-10 md:-left-32 md:-top-20 h-screen "
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
           fill="white"
         />
         <Spotlight
-          className="top-10 left-10 h-[80vh] w-[50vw] "
+          className="h-[80vh] w-[50vw] top-10 left-full"
           fill="purple"
         />
-        <Spotlight className="top-28 left-80 h-[80vh] w-[50vw] " fill="blue" />
+        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
+
+      <div
+        className="h-screen w-full dark:bg-black-100 bg-black-100 dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
+       absolute top-0 left-0 flex items-center justify-center"
+      >
+        <div
+          className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
+         bg-black-100 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+        />
       </div>
 
       <div className="flex justify-center relative my-20 z-10">
@@ -26,7 +36,7 @@ const Hero = () => {
 
           <TextGenerateEffect
             words="Transforming Concepts into Seamless User Experiences"
-            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+            className="text-center text-[40px] md:text-3xl lg:text-6xl"
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
